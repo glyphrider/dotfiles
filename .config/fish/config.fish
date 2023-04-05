b03aba9f48cee8a0fad151bc037b1449943679d0
Brian H. Ward <glyphrider@gmail.com>
2023-04-05 14:59:09 -0400

    updated fish prompt and transparency in neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set -gx LIBVIRT_DEFAULT_URI "qemu:///system"
 
 if status is-interactive
-  #fish_config theme choose coolbeans
+  # fish_config prompt save nim
+  # fish_config theme save 'ayu Dark'
 end
