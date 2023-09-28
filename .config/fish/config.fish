18fbe9eafc86d038bf6933b095ecb928037a4a93
Brian H. Ward <glyphrider@gmail.com>
2023-09-28 11:25:36 -0400

    update sway position for multimonitor; tweak some fish shell stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 set -gx LIBVIRT_DEFAULT_URI "qemu:///system"
 
+fish_add_path ~/.local/bin
+
 if status is-interactive
   # fish_config prompt save nim
   # fish_config theme save 'ayu Dark'
   starship init fish | source
 end
+
+shadowenv init fish | source
