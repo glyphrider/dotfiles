f7c2a5ddd3464e4af722ea22ba55c1f738bdbce8
Brian H. Ward <glyphrider@gmail.com>
2023-11-01 12:53:36 -0400

    flutter

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ set -gx LIBVIRT_DEFAULT_URI "qemu:///system"
 
 fish_add_path ~/.local/bin
 
+# flutter
+fish_add_path ~/.flutter/bin
+setenv CHROME_EXECUTABLE (which google-chrome-stable)
+
 if status is-interactive
   # fish_config prompt save nim
   # fish_config theme save 'ayu Dark'
